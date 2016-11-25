f0eada008ea7649661d10df764515fcdbf2744be
facingwaller <qinkaifang08@qq.com>
2016-11-25 01:07:10 -0800

    second commit

---
+++ client/test.lua~
@@ -0,0 +1,8 @@
+function sleep (n)
+   os.execute("sleep " .. n)
+end
+
+
+print "1"
+sleep(5)
+print "2"
---
+++ service/auth.lua
@@ -0,0 +1,59 @@
+local skynet = require "skynet"
+local service = require "service"
+local client = require "client"
+local log = require "log"
+
+local auth = {}
+local users = {}
+local cli = client.handler()
+
+local SUCC = { ok = true }
+local FAIL = { ok = false }
+-- add ender
+function CountTB (tb)
+ local count = 0
+  if  tb  then
+	for v in pairs(tb) do
+		count=count+1
+	end
+   end
+	return count
+end
+-- end
+function cli:signup(args)
+	log("signup userid = %s", args.userid)
+	if users[args.userid] then
+		return FAIL
+	else
+		users[args.userid] = true
+		--print (" signup users :"..CountTB(tb)) -- 打印当前人数
+		return SUCC
+	end
+end
+
+function cli:signin(args)
+	log("signin userid = %s", args.userid)
+	if users[args.userid] then
+		self.userid = args.userid
+		self.exit = true
+		--print (" signin users :"..CountTB(tb)) -- 打印当前人数
+		return SUCC
+	else
+		return FAIL
+	end
+end
+
+function cli:ping()
+	log("ping")
+end
+
+function auth.shakehand(fd)
+	local c = client.dispatch { fd = fd }
+	return c.userid
+end
+
+service.init {
+	command = auth,
+	info = users,
+	init = client.init "proto",
+}
