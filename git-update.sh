e6c061bc7e47d9946b6c20a8e0744b653789921a
facingwaller <qinkaifang08@qq.com>
2016-11-27 23:19:16 -0800

    add

--- git-update.sh
+++ git-update.sh
@@ -1,4 +1,5 @@
- git add client cservice lualib proto service git-update.sh read.me
+git pull 
+git add client cservice lualib proto service git-update.sh read.me
  
  git commit -m $1
  git push -u origin master
